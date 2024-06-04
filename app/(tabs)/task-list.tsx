33b6476e8c60acf00856436ed0357e50bb02f367
Facundo Ríos <facundorios2005@gmail.com>
2024-06-04 16:16:34 -0300

    feat: login & register

--- app/(tabs)/task-list.tsx
+++ app/(tabs)/task-list.tsx
@@ -2,15 +2,19 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Button, Icon } from "react-native-paper";
 import { styles } from ".";
+import { Link } from "expo-router";
+
 
 
 export default function TodoListScreen() {
   return (
     <ThemedView>
       <ThemedText  type="title">Todo List</ThemedText>
+      <Link href="todoForm">
       <Button icon="account-cowboy-hat" style={styles.button}>
         Añadir to do
       </Button>
+      </Link>
     </ThemedView>
   );
 }
